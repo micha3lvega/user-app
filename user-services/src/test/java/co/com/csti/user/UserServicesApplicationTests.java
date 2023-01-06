c884fe0cc73dadbadffa66e418f9b8ed0f7484ec
Michael Vega <micha3lvega@gmail.com>
2023-01-05 20:37:17 -0500

    Creacion de la estructura basica del proyecto

---
+++ user-services/src/test/java/co/com/csti/user/UserServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.csti.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

f619ea7db236f6da270f9e59a8d09bbbb8423606
Michael Vega <micha3lvega@gmail.com>
2023-01-13 17:32:29 -0500

    Pruebas unitaras

--- user-services/src/test/java/co/com/csti/user/controller/UserControllerTest.java
+++ user-services/src/test/java/co/com/csti/user/controller/UserControllerTest.java
@@ -1,17 +1,29 @@
 package co.com.csti.user.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Random;
 
 import javax.validation.ConstraintViolationException;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 
 import co.com.csti.user.integration.dto.UserDTO;
 
@@ -21,15 +33,37 @@ class UserControllerTest {
 	@Autowired
 	private UserController controller;
 
+	private TestRestTemplate restTemplate;
+
+	@LocalServerPort
+	int randomServerPort;
+
+	private static final Logger log = LoggerFactory.getLogger(UserControllerTest.class);
+
+	String url;
+
+	@BeforeEach
+	void before() {
+		restTemplate = new TestRestTemplate();
+
+		url = "http://localhost:" + randomServerPort + "/api/v1/user";
+		log.info("url: {}", url);
+
+	}
+
 	@Test
 	@DisplayName("Valida que al enviar un paginado se retornen solo el numero de registros solictados")
 	void testPageable() {
 
-		var pageable = PageRequest.of(0, 10);
+		// Generar el tamaño de la pagina al azar
+		var rand = new Random();
+		var size = rand.nextInt(20);
 
-		var findAll = controller.findAll(pageable);
+		var pageable = PageRequest.of(0, size);
 
-		assertEquals(10, findAll.getNumberOfElements());
+		var page = controller.findAll(pageable);
+
+		assertEquals(size, page.getNumberOfElements());
 
 	}
 
@@ -112,6 +146,12 @@ class UserControllerTest {
 		assertNotNull(newUser);
 		assertNotNull(newUser.getId());
 
+		// Buscar si se añadio correctamente
+		var findUser = controller.findByID(newUser.getId());
+
+		assertNotNull(findUser);
+		assertNotNull(findUser.getId());
+
 	}
 
 	@Test
@@ -123,7 +163,7 @@ class UserControllerTest {
 		var user = UserDTO.builder().email(randomEmail).name("name").lastName("lastname").mobileNumber("3000000000")
 				.password("123456").build();
 
-		var newUser = controller.insert(user);
+		var newUser = restTemplate.postForObject(url, user, UserDTO.class);
 
 		assertNotNull(newUser);
 		assertNotNull(newUser.getId());
@@ -132,12 +172,59 @@ class UserControllerTest {
 		var newname = "newname";
 		newUser.setName(newname);
 
-		var updateUser = controller.update(newUser);
+		var headers = new HttpHeaders();
+		var requestEntity = new HttpEntity<>(newUser, headers);
+		var updateUser = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, UserDTO.class);
+
+		log.info("updateUser: {}", updateUser);
 
 		assertNotNull(updateUser);
-		assertNotNull(updateUser.getId());
-		assertEquals(newname, updateUser.getName());
+		assertNotNull(updateUser.getBody());
+		assertNotNull(updateUser.getBody().getId());
+		assertEquals(newname, updateUser.getBody().getName());
 
 	}
 
+	@Test
+	@DisplayName("Prueba para validar si se inactiva el usuario")
+	void testInactive() {
+
+		// Buscar el usuario por el correo
+		var findUser = restTemplate.getForObject(url + "/email/me@example.com", UserDTO.class);
+
+		assertNotNull(findUser);
+		assertNotNull(findUser.getId());
+
+		// Inactivar el usuario
+		var disableUrl = url + "/" + findUser.getId();
+		log.info("disableUrl: {}", disableUrl);
+		restTemplate.delete(disableUrl);
+
+		// Buscar nuevamente el usuario
+		var inactiveUser = restTemplate.getForObject(url + "/" + findUser.getId(), UserDTO.class);
+
+		// Validar que el estado del usuario haya cambiado
+		assertNotNull(inactiveUser);
+		assertNotNull(inactiveUser.getId());
+		assertFalse(inactiveUser.isActive());
+
+	}
+
+	@Test
+	@DisplayName("Prueba para validar si se activa el usuario")
+	void testActive() {
+
+		// Buscar el usuario por el correo
+		var findUser = restTemplate.getForObject(url + "/email/me@example.com", UserDTO.class);
+
+		// activa el usuario
+		var activeUrl = url + "/active/" + findUser.getId();
+		var activeUser = restTemplate.postForObject(activeUrl, null, UserDTO.class);
+
+		// Validar que el estado del usuario haya cambiado
+		assertNotNull(activeUser);
+		assertNotNull(activeUser.getId());
+		assertTrue(activeUser.isActive());
+
+	}
 }

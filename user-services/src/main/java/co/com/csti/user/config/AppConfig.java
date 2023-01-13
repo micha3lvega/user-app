47870d30c623f9ca1362afb54e3dfcc912d8e582
Michael Vega <micha3lvega@gmail.com>
2023-01-12 19:09:07 -0500

    Agregar dependencias para encripcion de contraseñas

--- user-services/src/main/java/co/com/csti/user/config/AppConfig.java
+++ user-services/src/main/java/co/com/csti/user/config/AppConfig.java
@@ -3,6 +3,8 @@ package co.com.csti.user.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
@@ -10,6 +12,11 @@ public class AppConfig {
 	@Bean
 	public ModelMapper mapper() {
 		return new ModelMapper();
+	}
+
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }

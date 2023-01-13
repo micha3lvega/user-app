9b9f432bdc6d8b4110845915adcb6ec6cfab0d81
Michael Vega <micha3lvega@gmail.com>
2023-01-12 23:18:37 -0500

    Creacion de pruebas unitarias

---
+++ user-services/src/test/java/co/com/csti/user/controller/UserControllerTest.java
@@ -0,0 +1,121 @@
+package co.com.csti.user.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.validation.ConstraintViolationException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.data.domain.PageRequest;
+
+import co.com.csti.user.integration.dto.UserDTO;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class UserControllerTest {
+
+	@Autowired
+	private UserController controller;
+
+	@Test
+	@DisplayName("Valida que un usuario se cree correctamente")
+	void tesAddUserSuccess() {
+
+		var randomEmail = System.currentTimeMillis() + "@email.com";
+
+		var user = UserDTO.builder().email(randomEmail).name("name").lastName("lastname").mobileNumber("3000000000")
+				.password("123456").build();
+
+		var newUser = controller.insert(user);
+
+		log.info("Nuevo usuario: {}", newUser);
+
+		assertNotNull(newUser);
+		assertNotNull(newUser.getId());
+
+	}
+
+	@Test
+	@DisplayName("Valida que al no ingresar un email se genere error")
+	void testErrorUserInvalidEmail() {
+
+		// No email
+		var user = UserDTO.builder().name("name").lastName("lastname").mobileNumber("3000000000").password("123456")
+				.build();
+
+		var exception = Assertions.assertThrows(ConstraintViolationException.class, () -> {
+			controller.insert(user);
+		});
+
+		Assertions.assertEquals("insert.user.email: El correo es obligatorio", exception.getMessage());
+
+	}
+
+	@Test
+	@DisplayName("Valida que si el formato del correo no es correo sea invalido")
+	void testErrorUserInvalidEmailFormat() {
+
+		// No email
+		var user = UserDTO.builder().name("name").email("email").lastName("lastname").mobileNumber("3000000000").password("123456")
+				.build();
+
+		var exception = Assertions.assertThrows(ConstraintViolationException.class, () -> {
+			controller.insert(user);
+		});
+
+		log.info("insert.user.email: {}", exception.getMessage());
+
+		Assertions.assertEquals("insert.user.email: debe ser una dirección de correo electrónico con formato correcto", exception.getMessage());
+
+	}
+
+	@Test
+	@DisplayName("Valida que al no ingresar un nombre se genere error")
+	void testErrorUserInvalidName() {
+
+		// No email
+		var user = UserDTO.builder().email("me@example.com").lastName("lastname").mobileNumber("3000000000").password("123456")
+				.build();
+
+		var exception = Assertions.assertThrows(ConstraintViolationException.class, () -> {
+			controller.insert(user);
+		});
+
+		Assertions.assertEquals("insert.user.name: El nombre es obligatorio", exception.getMessage());
+
+	}
+
+	@Test
+	@DisplayName("Valida que al no ingresar un apellido se genere error")
+	void testErrorUserInvalidLastName() {
+
+		// No email
+		var user = UserDTO.builder().email("me@example.com").name("name").mobileNumber("3000000000").password("123456")
+				.build();
+
+		var exception = Assertions.assertThrows(ConstraintViolationException.class, () -> {
+			controller.insert(user);
+		});
+
+		Assertions.assertEquals("insert.user.lastName: El apellido es obligatorio", exception.getMessage());
+
+	}
+
+	@Test
+	@DisplayName("Valida que al enviar un paginado se retornen solo el numero de registros solictados")
+	void testPageable() {
+
+		var pageable = PageRequest.of(0, 10);
+
+		var findAll = controller.findAll(pageable);
+
+		assertEquals(findAll.getNumberOfElements(), 10);
+
+	}
+}
